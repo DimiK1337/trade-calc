4c6b155e6a10d4600d8b806e99d522089f38982d
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2026-01-10 23:48:21 +0100

    Session.py: Added event method to enforce FK constraints when using SQLIte

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,16 +1,26 @@
-from sqlalchemy import create_engine
+# app/db/session.py
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
 
 from app.core.config import settings
 
 # SQLite needs this in multithreaded apps (FastAPI dev server)
-connect_args = {"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {}
+isDB_SQLite = settings.DATABASE_URL.startswith("sqlite")
+connect_args = {"check_same_thread": False} if isDB_SQLite  else {}
 
 engine = create_engine(
   settings.DATABASE_URL, 
   connect_args=connect_args, 
   future=True
 )
+
+if isDB_SQLite:
+  @event.listens_for(Engine, "connect")
+  def _set_sqlite_pragma(dbapi_connection, connection_record):
+      cursor = dbapi_connection.cursor()
+      cursor.execute("PRAGMA foreign_keys=ON")
+      cursor.close()
 
 SessionLocal = sessionmaker(
   bind=engine, 
